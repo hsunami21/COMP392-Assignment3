42211e0b728b4bb50d94bc8154475dc902fce581
Wendall Hsu <Wendall@Wendalls-MacBook-Pro.local>
2016-03-07 20:56:38 -0500

    Initial commit

---
+++ Scripts/objects/keyboardcontrols.js
@@ -0,0 +1,14 @@
+var objects;
+(function (objects) {
+    // KeyboardControls Class +++++++++++++++
+    var KeyboardControls = (function () {
+        // PUBLIC INSTANCE VARIABLES ++++++++++++
+        // CONSTRUCTOR ++++++++++++++++++++++++++    
+        function KeyboardControls() {
+        }
+        return KeyboardControls;
+    }());
+    objects.KeyboardControls = KeyboardControls;
+})(objects || (objects = {}));
+
+//# sourceMappingURL=keyboardcontrols.js.map
---
+++ Scripts/objects/keyboardcontrols.ts
@@ -0,0 +1,11 @@
+module objects {
+    // KeyboardControls Class +++++++++++++++
+    export class KeyboardControls {
+        // PUBLIC INSTANCE VARIABLES ++++++++++++
+    
+        // CONSTRUCTOR ++++++++++++++++++++++++++    
+        constructor() {
+
+        }
+    }
+}
